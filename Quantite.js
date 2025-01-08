27aa423a521a2b71fb48cc3d0b6583d0b7fe00ed
LevyPouliche <caribouya23@gmail.com>
2025-01-08 14:10:25 -0500

    Add files via upload

--- Quantite.js
+++ Quantite.js
@@ -1,17 +1,17 @@
 function incrementerNombre(button) {
-  var gridItem = button.parentElement;
-  var nombreSpan = gridItem.querySelector(".nombre");
-  var nombre = parseInt(nombreSpan.textContent);
-  nombre++;
-  nombreSpan.textContent = nombre;
-}
-
-function decrementerNombre(button) {
-  var gridItem = button.parentElement;
-  var nombreSpan = gridItem.querySelector(".nombre");
-  var nombre = parseInt(nombreSpan.textContent);
-  if (nombre > 0) {
-    nombre--;
+    var gridItem = button.parentElement;
+    var nombreSpan = gridItem.querySelector(".nombre");
+    var nombre = parseInt(nombreSpan.textContent);
+    nombre++;
+    nombreSpan.textContent = nombre;
   }
-  nombreSpan.textContent = nombre;
-}
+  
+  function decrementerNombre(button) {
+    var gridItem = button.parentElement;
+    var nombreSpan = gridItem.querySelector(".nombre");
+    var nombre = parseInt(nombreSpan.textContent);
+    if (nombre > 0) {
+      nombre--;
+    }
+    nombreSpan.textContent = nombre;
+  }
